994e3391c98d72a251d2d6c4cf545ce42e124d00
Lino Coltura <lino.coltura@hotmail.com>
2017-10-17 15:51:22 +0200

    Activities temp. on profile

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -15,4 +15,17 @@
     @endif
     <p>{{ $user->email }}</p>
 
+    <h2>Activities</h2>
+
+    @foreach($runs as $r)
+        <div class="panel panel-default">
+    <h3>{{ $r->name }}</h3>
+    <h4>{{ $r->date }}</h4>
+    <p>{{ $r->km . " km" }}</p>
+    <p>{{ $r->minutes . " min"  }}</p>
+    <p>{{ $r->average_speed . " m/s" }}</p>
+    <p>{{ $r->max_speed . " m/s" }}</p>
+        </div>
+    @endforeach
+
 @endsection
