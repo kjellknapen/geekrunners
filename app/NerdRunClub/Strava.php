84bc73cce95ca3a36a8b7bdd0b03594244a47a2e
Kjell <knapenkjell@gmail.com>
2017-10-18 11:12:39 +0200

    strava static

--- app/NerdRunClub/Strava.php
+++ app/NerdRunClub/Strava.php
@@ -14,29 +14,29 @@ use Psy\Util\Str;
 
 class Strava
 {
-    private $client;
-    private $client_id;
-    private $client_secret;
-    private $redirect_uri;
+    private static $client;
+    private static $client_id;
+    private static $client_secret;
+    private static $redirect_uri;
     
     public function __construct()
     {
-        $this->client = new Client();
-        $this->client_id = $_ENV['STRAVA_KEY'];
-        $this->client_secret = $_ENV['STRAVA_SECRET'];
-        $this->redirect_uri = $_ENV['STRAVA_REDIRECT_URI'];
+        self::$client = new Client();
+        self::$client_id = $_ENV['STRAVA_KEY'];
+        self::$client_secret = $_ENV['STRAVA_SECRET'];
+        self::$redirect_uri = $_ENV['STRAVA_REDIRECT_URI'];
     }
 
-    public function redirect(){
-        return redirect('https://www.strava.com/oauth/authorize?client_id=20590&response_type=code&redirect_uri='. $this->redirect_uri .'&state=mystate&approval_prompt=force');
+    public static function redirect(){
+        return redirect('https://www.strava.com/oauth/authorize?client_id=20590&response_type=code&redirect_uri='. self::$redirect_uri .'&state=mystate&approval_prompt=force');
     }
 
-    public function tokenExchange($code){
+    public static function tokenExchange($code){
         //$url = "'https://www.strava.com/oauth/token?client_id=20594&client_secret=426f99ae57f2c243fdcc6e5fa320c011523c6161&code=".$code."'";
-        $res = $this->client->request('POST', 'https://www.strava.com/oauth/token', [
+        $res = self::$client->request('POST', 'https://www.strava.com/oauth/token', [
             'form_params' => [
-                'client_id' => '20590',
-                'client_secret' => 'f547d41bb416342504f68b64aa24216544d3154d',
+                'client_id' => self::$client_id,
+                'client_secret' => self::$client_secret,
                 'code' => $code,
             ]
         ]);
@@ -45,13 +45,13 @@ class Strava
         return $result;
     }
 
-    public function post(){
+    public static function post(){
 
     }
 
-    public function get( $url, $config ){
+    public static function get( $url, $config ){
 
-        $res = $this->client->get( $url, $config );
+        $res = self::$client->get( $url, $config );
 
         $result = \GuzzleHttp\json_decode($res->getBody()->getContents());
 
