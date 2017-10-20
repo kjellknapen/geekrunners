02c7b127760b953bbd336bf956e0163a618984f0
Kjell <knapenkjell@gmail.com>
2017-10-20 17:29:35 +0200

    Added retrieveActivities to Request class

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,18 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Activity;
-use NerdRunClub\Calculations;
-use NerdRunClub\Facades\Strava;
 
 
 class UserController extends Controller
 {
     //
     public function index(){
-        Calculations::retrieveActivities();
+        \NerdRunClub\Request::retrieveActivities();
         $result = Activity::all()->where('user_id', Auth::id());
 
         $user = Auth::user();
