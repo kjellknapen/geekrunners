69c341229dfb9c5ba626c19a93ccd4d149d10b0b
Lino Coltura <lino.coltura@hotmail.com>
2017-11-16 12:09:49 +0100

    more realistic data on users & activities

--- database/factories/ActivityFactory.php
+++ database/factories/ActivityFactory.php
@@ -10,11 +10,12 @@ $factory->define(App\Activity::class, function (Faker $faker) {
       'user_id'=> $user_id[rand(1, count($user_id)-1)],
       'name'=> $faker->name,
       'strava_id'=> str_random(10),
-        'date'=> new DateTime(),
+      'date'=> new DateTime(),
       'map_id'=> str_random(10),
       'average_speed'=>$faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 10),
       'max_speed'=>$faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 15),
-      'km'=>$faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100),
-      'minutes'=>$faker->randomNumber($nbDigits = 2, $strict = false),
+      'km'=>$faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 15),
+//      'minutes'=>$faker->randomNumber($nbDigits = 2, $strict = false),
+      'minutes'=>$faker->randomFloat($nbMaxDecimals = 2, $min = 15, $max = 45),
     ];
 });
