186baf611482defd93c19af7b2c0fe8bc41149fb
Lino Coltura <lino.coltura@hotmail.com>
2017-11-29 22:20:47 +0100

    ａｅｓｔｈｅｔｉｃ  update on splash screen

--- resources/views/splash.blade.php
+++ resources/views/splash.blade.php
@@ -4,7 +4,10 @@
 <div class="splash">
     <div class="center">
             <div class="splash-panel">
+
                 <h1 class="nerdun-title">GeekRunners</h1>
+                <img src="/img/logo.svg" alt="" class="splash-logo">
+
                 <p class="splash-p">IMD's very own training platform.</p><p> Check on how your fellow geeks are doing & track your own progress. Reach the goals and earn shiny medals. <span class="sub-title">Let's do this!</span></p>
                 <a href="/login" class="strava-login"><button type="button" class="btn btn-lg">Login with Strava</button></a>
             </div>
