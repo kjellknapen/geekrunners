4c282bd1f173f66f43f7e309d1415093e3377751
Lino Coltura <lino.coltura@hotmail.com>
2017-11-29 21:14:28 +0100

    rolepicker ux

--- resources/views/admin/chooserole.blade.php
+++ resources/views/admin/chooserole.blade.php
@@ -5,10 +5,13 @@
 @endsection
 
 @section('content')
+    <section class="half half-left">
     <h1>I am a</h1>
+        <br>
     @isset($error)
         <p class="alert alert-danger">{{ $error }}</p>
     @endisset
+        <br>
     <div>
         <form action="{{ action('AdminController@saveRole') }}" method="post" class="role-form">
             {{ csrf_field() }}
@@ -24,9 +27,10 @@
                 <label for="password">Teacher's Password</label>
                 <input type="password" name="password" class="form-control" id="password" placeholder="Password">
             </div>
-            <input type="submit" class="btn" value="Submit">
+            <input type="submit" class="btn btn-lg geek-button" value="Submit">
         </form>
     </div>
+    </section>
 @endsection
 
 @section('scripts')
