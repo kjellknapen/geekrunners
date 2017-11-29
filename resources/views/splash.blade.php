980bf192e81ac6b4045643f4713a8dad3b5a4b27
Lino Coltura <lino.coltura@hotmail.com>
2017-11-29 21:08:00 +0100

    splash page ux

--- resources/views/splash.blade.php
+++ resources/views/splash.blade.php
@@ -3,8 +3,9 @@
 
 <div class="splash">
     <div class="center">
-        <h1 class="nerdun-title">GeekRunners</h1>
-            <div class="panel-body">
+            <div class="splash-panel">
+                <h1 class="nerdun-title">GeekRunners</h1>
+                <p class="splash-p">IMD's very own training platform.</p><p> Check on how your fellow geeks are doing & track your own progress. Reach the goals and earn shiny medals. <span class="sub-title">Let's do this!</span></p>
                 <a href="/login" class="strava-login"><button type="button" class="btn btn-lg">Login with Strava</button></a>
             </div>
     </div>
