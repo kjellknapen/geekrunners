852f024408ebbef131460d9afa7dfbaa782a893f
Joris-Keirsmeakers <r0297332@student.thomasmore.be>
2017-12-05 23:55:28 +0100

    Schedule creation done

--- app/NerdRunClub/ScheduleCalculations.php
+++ app/NerdRunClub/ScheduleCalculations.php
@@ -3,6 +3,7 @@
 namespace NerdRunClub;
 
 use App\schedules;
+use App\Event;
 
 class ScheduleCalculations
 {
@@ -24,14 +25,11 @@ class ScheduleCalculations
     return $sets;
     }
 
-    public function calculate_speed(){
-      $speed = 7;
-      return $speed;
-    }
-
     public function create_schedule(){
       $weeks =  $this->calculate_weeks();
       $sets = $this->calculate_sets();
+      $event = event::find(1);
+      $distance = $event->distance;
       //1) Create a DB entry per week
       for ($i=0; $i < $weeks; $i++) {
         $schedule = new schedules;
@@ -39,34 +37,50 @@ class ScheduleCalculations
         $week = $i+1;
         $schedule->week = $week;
         //determine set;
-        $set = ceil($i/3);
+        $set = ceil($week/3);
         $schedule->set = $set;
+
+        //speed goes up per third of training completed
+        if ($set <= $sets/3) {
+          $speed = 7;
+        }
+        elseif ($set>$sets/3 && $set <= $sets*(2/3)) {
+          $speed = 8;
+        }
+        else {
+          $speed = 9
+          ;
+        }
+        $schedule->frequency_goal = 3;
+        $schedule->distance_goal =  round($distance/$sets * $set);
         //Position inside set determines all other params
         switch ($week%3) {
-          case 1:
-            //Frequency differs only for last case
-            $frequency = 3;
-            $schedule->frequency_goal = $frequency
-
-
-            break;
 
           case 2:
-            $schedule->frequency_goal = 3;
+            //distance ramps up during intense training week
+            $schedule->distance_goal =  round($distance/$sets * $set)+1;
             break;
 
           case 0:
+            //Only two sessions in the recuperation weeks
             $schedule->frequency_goal = 2;
             break;
-
-          default:
-            $schedule->frequency_goal = 0;
-            $schedule->distance_goal = 10;
-            $schedule->duration_goal = 0;
-            break;
         }
-        $schedule->duration_goal = 0;
-        $schedule->distance_goal =  $weeks/$sets * 1;
+
+        //last week doesn't get training increase -> already at max distance + only one session to warm up for the big day.
+        if ($week == $weeks) {
+          $schedule->distance_goal =  round($distance/$sets * $set);
+          $schedule->frequency_goal = 1;
+        }
+
+          $frequency = $schedule->frequency_goal;
+          $distance_goal = $schedule->distance_goal;
+          $schedule->duration_goal = 60/($speed/$distance_goal) * $frequency;
+          $duration =   $schedule->duration_goal;
+          //Rework into hours - minutes because noone uses 350 minutes.
+          //$hours = intval($duration/60);
+          //$minutes = $duration - ($hours * 60);
+
         $schedule->save();
       }
     }
