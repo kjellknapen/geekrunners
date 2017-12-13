9c47123172e1e8dbcd03ab80aa6e4e84dbc0b8df
Joris-Keirsmeakers <r0297332@student.thomasmore.be>
2017-12-13 09:31:39 +0100

    conflict didn't get resolved properly

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -14,7 +14,6 @@
 </div>
 <hr>
 
-<<<<<<< HEAD
   <h1 class="admin-title">Current event info</h1>
   <section>
     <p class="shedule-part" >Name of the event: {{$event['name']}}</p>
@@ -22,32 +21,6 @@
     <p class="shedule-part" >Date we start training: {{$event['start_date']}}</p>
     <p class="shedule-part" >Distance of the event: {{$event['distance']}}</p>
     <p class="shedule-part" >Location of the event: {{$event['location']}}</p>
-=======
-    <div class="form-group">
-      <label for="event-name">Event name</label>
-      <input type="text" class="form-control" name="event-name" id="event-name" placeholder="Event name" value="{{ !empty($event) ? $event->name : "" }}">
-    </div>
-    <div class="form-group">
-      <label for="event-date">Event date</label>
-      <input type="date" class="form-control" id="event-date" name="event-date" value="{{ !empty($event) ? $event->event_date : "" }}">
-    </div>
-    <div class="form-group">
-      <label for="start-date">The date we start training</label>
-      <input type="date" class="form-control" id="start-date" name="start-date" value="{{ !empty($event) ? $event->start_date : "" }}">
-    </div>
-    <div class="form-group">
-        <label for="distance">Distance we're training for (km)</label>
-        <input type="number" class="form-control" id="distance" name="distance" value="{{ !empty($event) ? $event->distance : "" }}">
-    </div>
-    <div class="form-group">
-      <label for="location">Location</label>
-      <input type="text" class="form-control" id="location" name="location" placeholder="Location" value="{{ !empty($event) ? $event->location : "" }}">
-    </div>
-    <input type="submit" class="btn btn-default" value="Set Event">
-  </form>
-  <br>
-  <hr>
->>>>>>> fe096797322ac5b343a0a1792238711ad0cb4137
 
   </section>
 
