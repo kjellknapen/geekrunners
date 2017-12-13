ed31b60a1c04373c78ab229789bb6fd77ae0e0de
Kjell <knapenkjell@gmail.com>
2017-12-13 09:38:47 +0100

    removed gender

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'id', 'firstname', 'lastname', 'email', 'avatar', 'gender', 'strava_id', 'token', 'notifications'
+        'id', 'firstname', 'lastname', 'email', 'avatar',  'strava_id', 'token', 'notifications'
     ];
 
     /**
