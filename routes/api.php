070495d3ec5e36c93eb8a545fc11a072fa623811
Kjell <knapenkjell@gmail.com>
2017-12-13 10:54:11 +0100

    update avatar

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/users', 'Api\UserApiController@index');
-
-Route::get('/users/{id}', 'Api\UserApiController@findById');
-
-Route::get('/schedules', 'Api\ScheduleController@index');
-
 Route::post('/schedules/calculate', 'Api\ScheduleController@calculate');
-
-Route::get('/schedules/{id}', 'Api\ScheduleController@findById');
