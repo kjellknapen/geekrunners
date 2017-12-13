720037627faa8fe382b4354a97c004051c3d5a69
Lino Coltura <lino.coltura@hotmail.com>
2017-12-13 11:23:19 +0100

    View setup (motivation)

--- app/Http/Controllers/MotivateController.php
+++ app/Http/Controllers/MotivateController.php
@@ -2,13 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Event;
+use App\EventWinners;
+use App\Schedules;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use NerdRunClub\Calculation;
 
 class MotivateController extends Controller
 {
 
-    public function index() {
-      return view('motivate');
+    public function index(Calculation $calculations) {
+
+
+        // Check if an enddate is set
+        if($calculations->getEndDate() != false || $calculations->getEndDate() != null){
+            $today = Carbon::now();
+            $enddate = $calculations->getEndDate();
+            $diff = $today->diffInDays($enddate);
+            //load the weektree & weekly goals
+            $currentWeek = $calculations->currentWeek();
+            if($currentWeek < count(Schedules::all())){
+                $event = $calculations->daysLeft();
+                $eventName = Event::take(1)->pluck('name');
+                return view('motivate', ['eventName'=> $eventName, 'event' => $event,'weekTree' => $calculations->weeklyGoalsTree(Auth::user(), $currentWeek),  'userStats' => $calculations->getUserStats(), 'D_Day' => false,
+                     'scheduleData' => $calculations->getScheduleData($currentWeek)]);
+            }else{
+                $event = $calculations->daysLeft();
+                $eventName = Event::take(1)->pluck('name');
+                $winners = EventWinners::all();
+                return view('motivate', ['eventName'=> $eventName, 'event' => $event, 'D_Day' => true, 'diff' => $diff,
+                    'topthreedday' => $winners]);
+            }
+        }
+
+
+        return view('motivate', ['empty' => true]);
     }
 
 }
--- resources/views/motivate.blade.php
+++ resources/views/motivate.blade.php
@@ -1,9 +1,75 @@
 @extends('layouts.master')
 
 @section('stylesheets')
-  <link rel="stylesheet" href="/css/admin.css">
+  <link rel="stylesheet" href="/css/screen.css">
 @endsection
 
 @section('content')
-<h1>RUN FOREST, RUN</h1>
+
+  <section class="motivate">
+    <h1>This week's goals</h1>
+
+      {{--<p> This week we'll run a {{ $scheduleData['distance_warmup']}} km session to warm up!</p>--}}
+      <ul class="goals">
+        @if($scheduleData['distance_completed'] >= 100)
+          <li class="goal-completed">&#10003; Reach {{ $scheduleData['distance_goal'] }} km in 1 session</li>
+        @elseif($scheduleData['distance_completed'] <= 0)
+          <li>&#10007; Reach {{ $scheduleData['distance_goal'] }} km in 1 session</li>
+        @else
+          <li>
+            <div class="radial-progress" data-progress="{{ $scheduleData['distance_completed'] }}">
+              <div class="circle">
+                <div class="mask full">
+                  <div class="fill"></div>
+                </div>
+                <div class="mask half">
+                  <div class="fill"></div>
+                  <div class="fill fix"></div>
+                </div>
+              </div>
+              <div class="inset"></div>
+            </div>
+            <p>Reach {{ $scheduleData['distance_goal'] }} km in 1 session</p>
+          </li>
+        @endif
+        @if($scheduleData['frequency_completed'] >= 100)
+          <li class="goal-completed">&#10003; Run {{ $scheduleData['frequency_goal'] }} times this week</li>
+        @elseif($scheduleData['frequency_completed'] <= 0)
+          <li>&#10007; Run {{ $scheduleData['frequency_goal'] }} times this week</li>
+        @else
+          <li>
+            <div class="radial-progress" data-progress="{{ $scheduleData['frequency_completed'] }}">
+              <div class="circle">
+                <div class="mask full">
+                  <div class="fill"></div>
+                </div>
+                <div class="mask half">
+                  <div class="fill"></div>
+                  <div class="fill fix"></div>
+                </div>
+              </div>
+              <div class="inset"></div>
+            </div>
+            <p>Run {{ $scheduleData['frequency_goal'] }} times this week </p>
+          </li>
+        @endif
+
+      </ul>
+      {{--<p class="time-ago tip">Tip: if you can run your warm-up and goal in under {{ $scheduleData['avg_duration'] }} minutes, you're right on track!</p>--}}
+      <div class="completed-users">
+        <p>{{ count($scheduleData['users_completed']) }} completed this</p>
+        <ul class="ul-users">
+          @foreach($scheduleData['users_completed'] as $key => $cuser)
+            @if($key <= 9)
+              <a href="/user/{{$cuser->id}}"><li class="img-completed tooltip" title="{{$cuser->firstname  . ' '  . $cuser->lastname}}"><img src="{{ $cuser->avatar }}" alt="{{$cuser->firstname  . ' '  . $cuser->lastname }}"></li></a>
+            @elseif($key == 10)
+              <a href="/user/{{$cuser->id}}"><li class="img-completed"><img src="/img/Other-users-icon.png" alt="{{$cuser->firstname  . ' '  . $cuser->lastname }}"></li></a>
+            @endif
+          @endforeach
+        </ul>
+      </div>
+
+  </section>
+
+
 @endsection
