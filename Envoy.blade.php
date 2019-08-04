a6c7658fb9e866a57c8af3b3e0de4585fdcaf7db
kjellknapen <knapenkjell@gmail.com>
2019-08-04 09:38:47 +0200

    updating migrations

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -45,9 +45,10 @@ git pull
 composer install
 yarn
 yarn run prod
-php artisan migrate --force
 php artisan config:clear
+php artisan migrate --force
 php artisan cache:clear
 php artisan config:cache
+php artisan config:clear
 sudo apachectl restart
 @endtask
